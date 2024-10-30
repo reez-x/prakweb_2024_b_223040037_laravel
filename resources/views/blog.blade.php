b56571716cc6162e245d0277fefe5cc328798402
reez 45 <rizkyabdurrahman455@gmail.com>
2024-10-30 11:16:33 +0700

    Video 4 - Blade Templating Engine

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -7,7 +7,7 @@
      @vite('resources/css/app.css')
      <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
      <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <title>Halaman Home</title>
+    <title>Blog</title>
 </head>
 <body class="h-full">
 
@@ -113,13 +113,13 @@
   
     <header class="bg-white shadow">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Blog</h1>
       </div>
     </header>
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
-        <p>Welcome to my home page</p>
+        <p>Welcome to my blog</p>
       </div>
     </main>
   </div>
