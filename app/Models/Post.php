94f0fe2a15617512cd207781865bbaed9a491f5c
reez 45 <rizkyabdurrahman455@gmail.com>
2024-11-02 20:57:05 +0700

    Video 9 - Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,35 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post{
-    public static function all(){
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Atrikel 1',
-                'author' => 'Reez',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nisi delectus rem nam, sunt, debitis libero dolores aperiam perspiciatis fugiat consectetur, excepturi cupiditate esse officiis modi architecto nemo magni deleniti.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Atrikel 2',
-                'author' => 'Reez',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nisi delectus rem nam, sunt, debitis libero dolores aperiam perspiciatis fugiat consectetur, excepturi cupiditate esse officiis modi architecto nemo magni deleniti.'
-            ]
-        ];
-}
-public static function find($slug): array{
-    // return Arr::first(Post::all(), function($post) use ($slug){
-    //     return $post['slug'] == $slug;
-    // });
-    $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-    if(!$post){
-        abort(404);
-    }
-    return $post;
-}
+class Post extends Model{
+    // protected $table = 'blog_posts';
+
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 };
