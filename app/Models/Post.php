b46a0a4d46d986e749ce9110857310b82d2efd4f
reez 45 <rizkyabdurrahman455@gmail.com>
2024-11-05 22:18:16 +0700

    Video 11 - Eloquent Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model{
     // protected $table = 'blog_posts';
     use HasFactory;
-    // protected $fillable = ['title', 'author', 'slug', 'body'];
+    protected $fillable = ['title', 'author', 'slug', 'body'];
+
+    public function author(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
 };
