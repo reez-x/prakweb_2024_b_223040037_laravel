e9b8138a42665ab38aaaccf53bb2cabc15dc2d43
reez 45 <rizkyabdurrahman455@gmail.com>
2024-11-06 10:31:01 +0700

    Video 12 - Post Category

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -30,9 +31,15 @@ Route::get('/contact', function () {
     return view('contact', ['title'=>'Contact']);
 });
 
-Route::get('/authors/{user}', function (User $user) {
+Route::get('/authors/{user:username}', function (User $user) {
 
     // $post = Post::find($id);
 
-    return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
+    return view('posts', ['title' => count($user->posts) . ' Articles by ' . $user->name, 'posts' => $user->posts]);
+});
+Route::get('/categories/{category:slug}', function (Category $category) {
+
+    // $post = Post::find($id);
+
+    return view('posts', ['title' => 'Articles in : ' . $category->name, 'posts' => $category->posts]);
 });
